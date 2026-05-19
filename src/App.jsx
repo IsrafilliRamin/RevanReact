cce9ac65715b60c9639d23b80053a20b5d97177f
Israfilli Ramin <106513116+IsrafilliRamin@users.noreply.github.com>
2026-05-19 19:55:12 +0400

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
   return (
 
     <div className='m-5 flex flex-col items-center justify-center gap-4'>
-<div>Revan git</div>
+<div>Ramin git</div>
  
       <FormTodo submitHandler={submitHandler} input={input} setInput={setInput} />
       <Todos todos={todos} />
